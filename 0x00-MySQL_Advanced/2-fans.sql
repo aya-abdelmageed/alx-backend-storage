22f40faad737305ff41c110b27059deaa3be01af
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-22 23:34:26 +0200

    2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,5 @@
+-- ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
