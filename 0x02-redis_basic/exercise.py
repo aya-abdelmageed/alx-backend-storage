ec380626e6fe8013f2ab28b3e2c257715c7703bb
aya-abdelmageed <aya.abdelmageed.01@gmail.com>
2024-01-24 23:31:56 +0200

    add tasks

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ from typing import Any, Callable, Optional, Union
 
 class Cache:
     """cache class"""
-    def __init__(self):
+    def __init__(self) -> None:
         """initialize new cache object"""
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -21,3 +21,32 @@ class Cache:
         client = self._redis
         client.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """
+        method that take a key string argument and an optional Callable argument named fn.
+        This callable will be used to convert the data back to the desired format.
+        """
+        client = self._redis
+        value = client.get(key)
+        if not value:
+            return
+        if fn is int:
+            return self.get_int(value)
+        if fn is str:
+            return self.get_str(value)
+        if callable(fn):
+            return fn(value)
+        return value
+
+    def get_str(self, data: bytes) -> str:
+        """
+        Converts bytes to string
+        """
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """
+        Converts bytes to integers
+        """
+        return int(data)
